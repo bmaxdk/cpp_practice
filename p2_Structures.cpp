4f0a4f57787774d54f5051cb30fb4cd53ce40bc4
bmaxdk <choh8uw@gmail.com>
2023-01-28 19:11:51 -0500

    Update p2

--- p2_Structures.cpp
+++ p2_Structures.cpp
@@ -1,9 +1,59 @@
 #include<iostream>
-// #include<stdio.h>
+#include<stdio.h>
+/*
+Structure
+1) Defining Structure
+2) Size of Structure
+3) Declaring a Structure
+4) Accessing Members
 
+Used ex. of rectangle, complex number, student
+*/
 using namespace std;
+
+struct Rectangle
+{
+    int length; // Each integer takes 2 bytes or it may take 4 bytes but let's assuming it as 2 bytes
+    int breadth;
+
+};
+
+// Complex Number a+ib
+struct Complex
+{
+    int real; //2
+    int img; //2
+};
+
+// Student
+struct Student
+{
+    int roll; //2
+    char name[25]; //character take 1byte  //25
+    char depth[15]; //department name //10
+    char address[50]; //50
+};
+
 
 int main()
 {
     //Structure
+    struct Rectangle r; // Declaration - r is occupying 4 bytes
+    struct Rectangle r2 = {10,5}; //Declaration plus initialization.
+    r.length = 3;
+    r.breadth = 4;
+    r2.length = 15;
+    r2.breadth = 10;
+    printf("Area of Rectangle r is %d\n", r.length*r.breadth);
+    printf("Area of Rectangle r2 is %d\n", r2.length*r2.breadth);
+
+    // Student   
+    struct Student s;
+    s.roll = 10;
+    s.name = "John";
+    s.depth = "Engineering";
+    s.address = "Seattle"; 
+
+    // printf("one student information: \nroll:%d\nname: %C\ndepartment: %C\naddress: %C\n",s.roll, s.name, s.depth, s.address);
+    return 0;
 }
