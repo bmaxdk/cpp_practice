63ed4cb6cad7234926bc44a768c46a4a18398959
bmaxdk <choh8uw@gmail.com>
2023-01-29 18:44:06 -0500

    Update p7

---
+++ p7_Parameter_Passing_Methods.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+// * call by reference
+void swap1(int &x, int &y)
+{
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+}
+
+// call by adddress
+void swap2(int *x, int *y)
+{
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+int main()
+{
+    int x,y;
+    x=1;
+    y=8;
+    swap1(x,y);
+    printf("Swap1\nx=%d\ny=%d\n", x, y);
+    swap2(&x,&y);
+    printf("Swap2\nx=%d\ny=%d\n", x, y);
+
+}
