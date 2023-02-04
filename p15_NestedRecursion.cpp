985f72d98b72df07c99bc079d3c63e207960b215
bmaxdk <choh8uw@gmail.com>
2023-02-04 15:49:12 -0500

    Update p15

--- p15_NestedRecursion.cpp
+++ p15_NestedRecursion.cpp
@@ -1,7 +1,18 @@
 /*
 Nested Recursion
+
 parameter it self is parameter. Recursion inside of recursion.
 
+Nested recursion is used when a function calls itself within another function that also calls the first function. 
+This type of recursion is used in algorithms where there are multiple, smaller problems that can be solved by breaking down the larger problem into smaller subproblems.
+
+Nested recursion is used when a problem can be divided into two or more subproblems that are similar in nature, but with a smaller input size. 
+In this case, the subproblems are solved recursively and the solutions are combined to solve the original problem.
+
+Examples of problems that can be solved using nested recursion include the calculation of the Fibonacci sequence, the calculation of factorials, and the generation of permutations and combinations.
+
+It is important to note that nested recursion can lead to stack overflow errors if the recursion depth is too deep. 
+Therefore, it is important to carefully analyze the problem and determine if nested recursion is the best solution, or if another approach, such as iterative or dynamic programming, would be more appropriate.
 
         fun(95)
          |
