b35f04b82b9f3dea2af173667813c6630345a202
bmaxdk <choh8uw@gmail.com>
2023-02-04 17:21:22 -0500

    Update p16

---
+++ p16_SumOfNaturalNumberUsingRecursion.cpp
@@ -0,0 +1,58 @@
+/*
+Sum of First n Natural Numbers
+
+The key here is understand how you decide large part and smaller part.
+
+EX.
+1 + 2 + 3 + 4 + 5 + 6 + 7
+1 + 2 + 3 + ...+ n
+
+This is basically
+sum(n) = 1 + 2 + 3 + ... + (n-1) + n
+sum(n) = sum(n-1) + n 
+Here you see how you break down to use in recursion method to solve.
+
+Make sure you focus large and smaller part!
+
+
+                0       n=0
+sum(n) = 
+          sum(n-1)+n    n>0
+
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int sum(int n)
+{
+    if (n==0)
+        return 0;
+    return sum(n-1)+n;
+}
+
+int sum2(int n)
+{
+    return n*(n+1)/2;
+}
+
+int sum3(int n)
+{
+    int i,s=0;
+    for(i=1; i<=n; i++)
+    {
+        s+=i;
+    }
+    return s;
+}
+int main()
+{
+    // For best case use formula instead of using recursion
+    cout<< sum2(10)<<endl; //time O(1) 
+
+    cout<< sum3(10)<<endl; // time O(n) 
+    cout<< sum(10)<<endl; //  time O(n) 
+    return 0;
+}
