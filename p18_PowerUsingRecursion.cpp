f050139c4781fe205ff8b7bb0ebcadccac34f4c3
bmaxdk <choh8uw@gmail.com>
2023-02-04 19:56:04 -0500

    Update p18

--- p18_PowerUsingRecursion.cpp
+++ p18_PowerUsingRecursion.cpp
@@ -11,6 +11,14 @@ pow(m,n) =
             pow(m,n-1) * m  n>0
 
 
+Reduce version:
+2^8 = (2^2)^4 Therefore, as long as n is even we can reduce.
+(m*m)^n/2
+
+If odd,
+2^9 = 2 * 2^8 = 2 * (2^2)^4
+m* (m*m)^(n-1)/2
+
 */
 
 #include<iostream>
@@ -24,6 +32,16 @@ int rExponent(int m, int n)
         return 1;
     return rExponent(m, n-1) * m;
 }
+
+int rExponent2(int m, int n)
+{
+    if (n==0)
+        return 1;
+    if (n%2 == 0)
+        return rExponent2(m*m, n/2);
+    return rExponent2(m*m, (n-1)/2) * m;
+}
+
 
 int iExponent(int m, int n)
 {
@@ -41,6 +59,8 @@ int iExponent(int m, int n)
 int main()
 {
     cout<<rExponent(4,8)<<endl;
+    cout<<rExponent2(4,8)<<endl;
+
     cout<<iExponent(4,8)<<endl;
     cout<<pow(4,8)<<endl;
 
