978840a5baa4acade7faac51fe3d976f654294d5
bmaxdk <choh8uw@gmail.com>
2023-02-05 13:34:46 -0500

    Update p19

--- p19_TaylorSeriesRecursion.cpp
+++ p19_TaylorSeriesRecursion.cpp
@@ -10,6 +10,14 @@ fact(n-1) * n = fact(n)
 pow(x, n-1) * x = pow(x,n)
 
 
+Multiplication usage here is n(n+1) = O(n^2)
+
+We can reduce.
+e^x = 1 + x/1! + (x^2)/2! + (x^3)/3! ... n terms
+1 + x/1 + (x^2)/(1*2) + (x^3)/(1*2*3)
+
+1 + x/1 [1 + x/2[1 + x/3[1 + x/4]]] 
+O(n)
 */
 
 
@@ -18,20 +26,48 @@ pow(x, n-1) * x = pow(x,n)
 
 using namespace std;
 
-double e(int x, int n)
+double re(int x, int n)
 {
     if (n==0)
         return 1;
     static double p=1, f=1;
     double r;
-    r = e(x, n-1);
+    r = re(x, n-1);
     p*=x;
     f*=n;
     return r + p/f;
 }
+
+
+double ieH(int x, int n)
+{
+    double s=1;
+
+    for(;n>0;n--)
+    {
+        s = 1+x*s/n;
+    }
+    return s;
+}
+
+double reH(int x, int n)
+{
+    static double s;
+    if (n == 0)
+        return s;
+    s = 1+(x*s)/n;
+    return reH(x, n-1);
+
+}
 int main()
 {
-    cout<<e(4, 40);
+    cout<<re(4, 40)<<endl;
+    cout<<ieH(4, 40)<<endl;
+    cout<<reH(4, 40)<<endl;
+    cout<<3.0*2.0/4.0<<endl;
+    cout<<3.0/4.0*2.0 <<endl;
+
+
 
     return 0;
 }
