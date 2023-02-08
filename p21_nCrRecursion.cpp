3a2d0cd5624d311c40b32d4f59b324e13ab737e9
bmaxdk <choh8uw@gmail.com>
2023-02-07 21:47:08 -0500

    Update p21

--- p21_nCrRecursion.cpp
+++ p21_nCrRecursion.cpp
@@ -62,9 +62,8 @@ int c(int n, int r)
 {
     // Check condition when n==r or r==0 we see that is equals to 1
     if((r==0)||(n==r))
-    {
         return 1;
-    }
+        
     return c(n-1,r-1)+c(n-1,r);
 }
 
