3ac12de62b02d747f106462feb43f3a4dc3c18bb
bmaxdk <choh8uw@gmail.com>
2023-02-09 21:37:09 -0500

    Update p26

--- p26_FormulasForNDArray.cpp
+++ p26_FormulasForNDArray.cpp
@@ -22,3 +22,32 @@ Col-major (right to left)
 Addr(A[i1][i2][i3][i4]) = L0 + (i4*d1*d2*d3 + i3*d1*d2 + i2*d1 + i1) * w
 */
 
+#include<iostream>
+
+using namespace std;
+
+//sizeof(int) =4
+int main()
+{
+    unsigned int x[4][3] = {{1,2,3}, {4,5,6}, {7,8,9}, {10,11,12}};
+    printf("%u, %u, %u\n", x+3, *(x+3), *(x+2)+3);
+    return 0;
+}
+
+/*
+L0 = 2000
+1,0  |2,4  |3,8
+4,12 |5,16 |6,20
+7,24 |8,28 |9,32
+10,36|11,40|12,44
+
+how to access 8?
+*(*(x+2)+1) == 8
+how about addres??
+(*(x+2)+1) ==2028
+
+so
+x+3 = 2036
+*(x+3) = 2036
+*(x+2)+3 = 2036
+*/
