ca701120054e75f3102b4194766e14fa5343b24e
bmaxdk <choh8uw@gmail.com>
2023-02-15 23:05:55 -0500

    Update p27

---
+++ p27_Inserting_in_an_Array.cpp
@@ -0,0 +1,21 @@
+/*
+Array ADT
+
+size 10
+Length = 6
+*/
+#include <iostream>
+
+using namespace std;
+
+// 1.Display
+// printf("%d", A[]);
+//    for (int i=0; i<length; i++)
+//        printf("%d", A[i]);
+
+// 2. Add(x)/Append(x)
+// A[Length] = x;
+// Length++;
+// Time O(1);
+
+// 3. Insert(index,x)
