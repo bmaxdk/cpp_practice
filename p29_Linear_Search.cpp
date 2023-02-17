f8d8d0f8e15ed1cfed489861abe314e28d07f49d
bmaxdk <choh8uw@gmail.com>
2023-02-16 23:39:42 -0500

    Update p29

--- p29_Linear_Search.cpp
+++ p29_Linear_Search.cpp
@@ -1,8 +1,28 @@
 /*
 Array ADT::Linear Search
+
+Given:
 Size = 10
 Length = 10
 A = {8,9,4,7,6,3,10,5,14,1}
      0,1,2,3,4,5,6, 7,8 ,9
+
+
+key = 5 //what we looking for where located in index 7 //search is successful
+key = 12 // Search is unsuccessful
+
+psudocode
+// Start to compare each element
+
+for(i=0; i<Length; i++)
+{
+    if(key == A[i])
+        return i;
+}
+return -1;
+
+
+//Time O(n)
+
 
 */
