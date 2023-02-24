029b3116ea0c4cf54cde41c83ac7c9aebba907f1
bmaxdk <choh8uw@gmail.com>
2023-02-24 18:58:41 -0500

    Update p35 not done

--- p35_SetOperations.cpp
+++ p35_SetOperations.cpp
@@ -217,6 +217,27 @@ public:
         
     }
 
+    Array Union(Array& B)
+    {
+        Array C(length+B.length, length+B.length);
+        int i,j,k;
+        i=j=k=0;
+        while(i<length && j<B.length)
+        {
+            if(A[i]<B.Get(j))
+                C.Set(k++,A[i++]);
+            else
+                C.Set(k++,B.Get(j++));
+        }
+        for(;i<length;i++)
+            C.Set(k++,A[i]);
+        for(;j<length;j++)
+            C.Set(k++,B.Get(j));
+        
+        return C;
+        
+    }
+
     ~Array()
     {
         delete[] A;
